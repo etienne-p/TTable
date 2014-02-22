7ba7e570262bf7c71558cfd4372b08e1011e1134
etienne <etienne@etienne-K53SV.(none)>
2014-02-22 15:17:15 -0500

    refactoring, less flags, more functional

--- js/main.js
+++ js/main.js
@@ -16,55 +16,34 @@ var main = function() {
 	disc.setRadius(radius);
 	disc.render();
 
-	// TODO: redraw disc on fps IF NEEDED
-
-	// hard part UI movement to disc movement
-
-
-	// mouse control on disc
-
 	// translate mouse
 	var rect = canvas.getBoundingClientRect(),
 		canvasXOffset = rect.left,
 		canvasYOffset = rect.top,
 		prevPosition,
 		dAngle = 0,
-		motorStrength = 1,
-		userStrength = 0,
 		motorDAngle = 0.01,
-		//userDAngle = 0,
 		dist = GeomUtil.distance;
 
 	function toPolar(pos) {
 		return GeomUtil.cartesianToPolar({
-			x: pos.x - canvasXOffset - radius,
+			x: pos.x - canvasXOffset - radius, // TODO: find a proper way to manage relative coordinates
 			y: pos.y - canvasYOffset - radius
 		})
 	}
 
 	function watchMouse() {
-
-		// TODO: STOP WATCH DOES NOT HiTS DISC
-
-		// translate and change coordinates
 		p = toPolar(TTable.mouse.position.value);
-
-		motorStrength *= 0.9;
 		dAngle = p.angle - prevPosition.angle;
 		disc.angle += dAngle;
 		prevPosition = p;
+		disc.render();
 	}
 	// extraire l'angle
 
 	function moveDisc() {
-		// la variation de deplacement sur le radius influe sur le controle
-		// plus cette variation est faible plus on garde le controle
-		// inversement, on glisse et le moteur reprend de l'importance
-
-		// il va falloir travailler en coordonnees polaires
-		disc.angle += motorDAngle * motorStrength + dAngle * (1 - userStrength);
+		disc.angle += motorDAngle + dAngle;
 		dAngle *= 0.9; // represents an accumulation between frames
-		
 		disc.render();
 	}
 
@@ -72,13 +51,12 @@ var main = function() {
 
 	TTable.mouse.up.add(function() {
 		TTable.fps.tick.remove(watchMouse);
-		motorStrength = 1;
-		userStrength = 0;		
+		TTable.fps.tick.add(moveDisc);
 	});
 	TTable.mouse.down.add(function() {
 		// TODO: IF HiTS DISC
 		prevPosition = toPolar(TTable.mouse.position.value);
-		userStrength = 1;
+		TTable.fps.tick.remove(moveDisc);
 		TTable.fps.tick.add(watchMouse);
 	});
 
