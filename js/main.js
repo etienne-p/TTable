9d0d75f509258fbddf0aa72200d1658c4a9579fd
etienne <etienne@etienne-K53SV.(none)>
2014-03-01 11:40:28 -0500

    refactor, introduce AudioUtil

--- js/main.js
+++ js/main.js
@@ -1,42 +1,3 @@
-var audio = audio || {};
-
-audio.getContext = function() {
-	var rv = null;
-	if ('webkitAudioContext' in window) {
-		rv = new webkitAudioContext();
-	} else if ('AudioContext' in window) {
-		rv = new AudioContext();
-	}
-	audio.getContext = function() {
-		return rv;
-	}
-	return rv;
-}
-
-audio.loadSample = function(url_, callback_) {
-	var req = new XMLHttpRequest(),
-		onError = function() {
-			alert('decodeAudioData failed!');
-		},
-		loadCompleteHandler = function() {
-			audio.getContext().decodeAudioData(req.response, callback_, onError);
-		};
-	req.open('GET', url_, true);
-	req.responseType = 'arraybuffer';
-	req.addEventListener('load', loadCompleteHandler, false);
-	req.send();
-}
-
-audio.SamplePlayer = function(left_, right_) {
-
-}
-
-// buffer_ = __audioContext.createBuffer(__loadRequest.response, false);
-// smapler is only responsible for playing a buffer at a given rate
-// filters, gain, are to be handled externally
-
-//-- TODO
-
 var testScratch = function() {
 
 	// setup core
@@ -47,6 +8,7 @@ var testScratch = function() {
 	var info = document.getElementById('info');
 	canvas = document.createElement('canvas'),
 	disc = new TTable.Disc(canvas),
+	audio = TTable.AudioUtil,
 	radius = 400;
 
 	document.getElementsByTagName('body')[0].appendChild(canvas);
@@ -58,8 +20,6 @@ var testScratch = function() {
 			x: window.innerWidth * 0.5,
 			y: window.innerHeight * 0.5
 		}, radius);
-
-	
 
 	// AUDIO
 	audio.loadSample('media/loop.wav', function(buffer) {
---
+++ js/util/AudioUtil.js
@@ -0,0 +1,28 @@
+TTable.AudioUtil = {
+	getContext: function() {
+		var rv = null;
+		if ('webkitAudioContext' in window) {
+			rv = new webkitAudioContext();
+		} else if ('AudioContext' in window) {
+			rv = new AudioContext();
+		}
+		TTable.AudioUtil.getContext = function() {
+			return rv;
+		}
+		return rv;
+	},
+
+	loadSample: function(url_, callback_) {
+		var req = new XMLHttpRequest(),
+			onError = function() {
+				alert('decodeAudioData failed!');
+			},
+			loadCompleteHandler = function() {
+				audio.getContext().decodeAudioData(req.response, callback_, onError);
+			};
+		req.open('GET', url_, true);
+		req.responseType = 'arraybuffer';
+		req.addEventListener('load', loadCompleteHandler, false);
+		req.send();
+	}
+}
